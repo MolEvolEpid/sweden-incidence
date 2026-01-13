26b1eb023a5bc288d0656ed072e02f528b2ea597
Macauley Locke <58670342+MacauleyLockeml@users.noreply.github.com>
2026-01-13 07:51:00 -0700

    Update infer_inftimes_clu_new.R

--- scripts/CLU files/infer_inftimes_clu_new.R
+++ scripts/CLU files/infer_inftimes_clu_new.R
@@ -8,29 +8,12 @@ source("mbm.predict.scratch.R")
 #find individual from command line
 i <- commandArgs(trailingOnly = TRUE)
 i <- as.integer(i)
-i=1
+
 #load dataframe
 load("df.incidence.informed.1B.Rdata")
-
+df <- df.informed[i,]
 #subset patient of interest
-for (i in 1:100){
-    df <- df.informed[i,]
-    
-    if (length(df$CD4_qc[[1]])<4){
-      df$CD4_qc[[1]]<-df$CD4_qc[[1]][1:length(df$CD4_qc[[1]])]
-      df$CD4_dates[[1]]<-df$CD4_dates[[1]][1:length(df$CD4_qc[[1]])]
-    }else{
-      df$CD4_qc[[1]]<-tail(df$CD4_qc[[1]],4)
-      df$CD4_dates[[1]]<-tail(df$CD4_dates[[1]],4)
-    }
-    temp_cd4<-df$CD4_qc[[1]]
-    temp_cd4_dates<-df$CD4_dates[[1]]
 
-    for (j in 1:length(df$CD4_qc[[1]])){
-      df$CD4_qc[[1]]<-temp_cd4[j]
-      print(df$CD4_qc[[1]])
-      df$CD4_dates[[1]]<-temp_cd4_dates[j]
-      print(df$CD4_dates[[1]])
 
 
 infection.ages <- run.mbm(df, 
@@ -40,6 +23,5 @@ infection.ages <- run.mbm(df,
                           prior.type = 1, 
                           overall.seed = 20230424+i)
 
-saveRDS(infection.ages, file = paste0("~/Documents/New_incidence_work/Training_Data/single_CD4/",j,"M/infection.ages_", i, ".rds"))
-  }
-}
+saveRDS(infection.ages, file = paste0("~/infection.ages_", i, ".rds"))
+
